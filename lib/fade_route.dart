d3c7b42bba30b91b977a7b2247156f70db72e303
Kamash <93958137+AbdallahKamash@users.noreply.github.com>
2023-08-06 17:40:56 +0200

    init

--- lib/fade_route.dart
+++ lib/fade_route.dart
@@ -1,37 +1,27 @@
 import 'package:flutter/material.dart';
 
-class FadeRoute<T> extends PageRoute<T> {
-  FadeRoute(this.child);
-  @override
-  // TODO: implement barrierColor
-  Color get barrierColor => Colors.black;
+class FadeRoute extends PageRouteBuilder {
+  final Widget widget;
 
-  @override
-  String? get barrierLabel => null;
-
-  final Widget child;
-
-  @override
-  Widget buildPage(BuildContext context, Animation<double> animation,
-      Animation<double> secondaryAnimation) {
-    return FadeTransition(
-      opacity: animation,
-      child: child,
-    );
-  }
-
-  @override
-  bool get maintainState => true;
-
-  @override
-  Duration get transitionDuration => const Duration(milliseconds: 500);
+  FadeRoute({required this.widget})
+      : super(
+            transitionDuration: const Duration(seconds: 1),
+            transitionsBuilder: (BuildContext context,
+                Animation<double> animation,
+                Animation<double> secAnimation,
+                Widget child) {
+              animation = CurvedAnimation(
+                curve: Curves.easeIn,
+                parent: animation,
+              );
+              return ScaleTransition(
+                alignment: Alignment.center,
+                scale: animation,
+                child: child,
+              );
+            },
+            pageBuilder: (BuildContext context, Animation<double> animation,
+                Animation<double> secAnimation) {
+              return widget;
+            });
 }
-
-
-late final Animation<Offset> _offsetAnimation = Tween<Offset>(
-  begin: Offset.zero,
-  end: const Offset(1.5, 0.0),
-).animate(CurvedAnimation(
-  parent: _controller,
-  curve: Curves.elasticIn,
-));
